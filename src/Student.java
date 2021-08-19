6eac08929c610e81d7eafd9cf7014e80dd6ac60f
pelin <pelin.donat.3919@gmail.com>
2021-08-19 19:46:15 +0200

    versiyon 2: not girisi yapildi

--- src/Student.java
+++ src/Student.java
@@ -2,4 +2,8 @@ public class Student {
     public static void main(String[] args) {
         System.out.println("Projenin 1. asamasi bitti.");
     }
+    public static void GiveNote()
+    {
+        System.out.println("Note Table");
+    }
 }
